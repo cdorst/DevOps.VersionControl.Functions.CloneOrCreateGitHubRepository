d62195bfa4125ca3d4676a148acdeed655091dee
Christopher Dorst <chris@dorst.email>
2018-01-03 06:03:24 -0600

    Add RepositoryCreatorOrCloner

---
+++ DevOps.VersionControl.Functions.CloneOrCreateGitHubRepository/RepositoryCreatorOrCloner.cs
@@ -0,0 +1,24 @@
+﻿using Common.Structures.HttpBasicAuthentication;
+using DevOps.VersionControl.Structures.GitCommitUser;
+using DevOps.VersionControl.Structures.GitHub;
+using System.Threading.Tasks;
+using static Common.Functions.ClearDirectory.DirectoryClearer;
+using static DevOps.VersionControl.Functions.CheckIfGitHubRepositoryExists.GitHubRepositoryExistenceChecker;
+using static DevOps.VersionControl.Functions.CreateAndInitDotNetGitHubRepository.RepositoryCreator;
+using static DevOps.VersionControl.Functions.RunGitCloneCommand.GitCloneCommandRunner;
+using static Metaproject.Users.CDorst.GitHubAccessToken.GitHubCredentials;
+
+namespace DevOps.VersionControl.Functions.CloneOrCreateGitHubRepository
+{
+    public static class RepositoryCreatorOrCloner
+    {
+        public static async Task CloneOrCreate(string directory, string name, string description, BasicAuthenticationCredentials? credentials = null, UserInfo? user = null)
+        {
+            credentials = credentials ?? CDorst;
+            var repository = new AccountRepository(credentials.Value.User, name);
+            Clear(directory);
+            if (await Exists(repository)) Clone(directory, $"{repository.Uri}.git");
+            else await CreateRepository(directory, name, description, credentials, user);
+        }
+    }
+}
